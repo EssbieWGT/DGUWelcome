8f6f444ffdbdc1d95c1a9f4c09624cf458ef99b4
EssbieWGT <35500754+EssbieWGT@users.noreply.github.com>
2018-10-27 22:51:04 -0400

    Create maxViewers.js

---
+++ maxViewers.js
@@ -0,0 +1,28 @@
+//Simple function to log the maximum viewers in a YouTube live event! 
+//While the event is live, copy the code below, open up your console (option+command+j in Chrome), and paste this code in. 
+//Will run automatically, updating you on the current max number of viewers as needed as long as the window is open. 
+
+function tracking() {
+
+maxViewers = ["0"]
+
+  myTimer()
+
+  var myVar = setInterval(myTimer, 5000);
+
+  function myTimer() {
+
+    var currentViewers = document.getElementsByClassName("view-count style-scope yt-view-count-renderer")[0].innerHTML
+    var viewerNumber = currentViewers.split(' ')
+
+
+    if (viewerNumber[0] > maxViewers[0]){
+      maxViewers[0] = viewerNumber[0]
+      console.log("Max viewers = " + maxViewers[0] + " at " + new Date())
+    } else {
+      maxViewers[0] = maxViewers[0]
+    }
+  }
+}
+
+tracking()
